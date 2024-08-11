c99cec029f95cbaff7bb189b047a845a9227f251
Pedro Henrique Vieira <peagahvieira2003@gmail.com>
2024-08-11 12:58:12 -0300

    :broom: Navbar tailwind duplicated classes removed

--- resources/views/dashboard/partials/navbar.blade.php
+++ resources/views/dashboard/partials/navbar.blade.php
@@ -46,7 +46,7 @@
         </a>
         <form action="#" method="GET" class="hidden md:block md:pl-2">
             <label for="topbar-search" class="sr-only">Search</label>
-            <div class="relative md:w-64 md:w-96">
+            <div class="relative md:w-64">
             <div
                 class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none"
             >
@@ -108,7 +108,7 @@
 
         <!-- Dropdown menu -->
         <div
-            class="hidden overflow-hidden z-50 my-4 max-w-sm text-base list-none bg-white rounded divide-y divide-gray-100 shadow-lg dark:bg-gray-700 dark:divide-gray-600 rounded-xl"
+            class="hidden overflow-hidden z-50 my-4 max-w-sm text-base list-none bg-white divide-y divide-gray-100 shadow-lg dark:bg-gray-700 dark:divide-gray-600 rounded-xl"
             id="apps-dropdown"
         >
             <div
@@ -325,7 +325,7 @@
 
         <!-- Dropdown menu -->
         <div
-            class="hidden z-50 my-4 w-56 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600 rounded-xl"
+            class="hidden z-50 my-4 w-56 text-base list-none bg-white divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600 rounded-xl"
             id="dropdown"
         >
             <div class="py-3 px-4">
